b97bc5c44219c19f5ef5c56ef2bfa2fd3dafe424
Stephan van Diepen <svdiepen@zilverline.com>
2016-08-19 10:07:31 +0200

    Add storybook with block story

---
+++ src/components/Block.js
@@ -0,0 +1,13 @@
+import React, { Component } from 'react';
+
+class Block extends Component {
+  render() {
+    return (
+      <div className="Block">
+        Block!
+      </div>
+    );
+  }
+}
+
+export default Block;
---
+++ src/stories/block.js
@@ -0,0 +1,8 @@
+import React from 'react';
+import { storiesOf, action } from '@kadira/storybook';
+import Block from '../components/Block';
+
+storiesOf('Block', module)
+  .add('square', () => (
+    <Block />
+  ));
