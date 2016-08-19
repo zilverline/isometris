e2b49775640ead9f1ada1edf87d2cb71175af5df
Bob Forma <bobforma@gmail.com>
2016-08-19 17:11:58 +0200

    Move base rotation to grid
    And fix tetriminos storybook.

--- src/stories/tetrimino.js
+++ src/stories/tetrimino.js
@@ -1,72 +1,20 @@
 import React from 'react';
 import { storiesOf, action } from '@kadira/storybook';
 import { I, J, L, O, S, T, Z } from '../components/Tetrimino';
+import { Grid } from '../components/Grid';
 
 storiesOf('Tetrimino', module)
   .add('basic tetriminos', () => (
-    <div>
-      <I />
-      <J />
-      <L />
-      <O />
-      <S />
-      <T />
-      <Z />
-    </div>
-  ))
-  .add('I with rotations', () => (
-    <div>
-      <I rotation={0} />
-      <I rotation={90} />
-      <I rotation={180} />
-      <I rotation={270} />
-    </div>
-  ))
-  .add('J with rotations', () => (
-    <div>
-      <J rotation={0} />
-      <J rotation={90} />
-      <J rotation={180} />
-      <J rotation={270} />
-    </div>
-  ))
-  .add('L with rotations', () => (
-    <div>
-      <L rotation={0} />
-      <L rotation={90} />
-      <L rotation={180} />
-      <L rotation={270} />
-    </div>
-  ))
-  .add('O with rotations', () => (
-    <div>
-      <O rotation={0} />
-      <O rotation={90} />
-      <O rotation={180} />
-      <O rotation={270} />
-    </div>
-  ))
-  .add('S with rotations', () => (
-    <div>
-      <S rotation={0} />
-      <S rotation={90} />
-      <S rotation={180} />
-      <S rotation={270} />
-    </div>
-  ))
-  .add('T with rotations', () => (
-    <div>
-      <T rotation={0} />
-      <T rotation={90} />
-      <T rotation={180} />
-      <T rotation={270} />
-    </div>
-  ))
-  .add('Z with rotations', () => (
-    <div>
-      <Z rotation={0} />
-      <Z rotation={90} />
-      <Z rotation={180} />
-      <Z rotation={270} />
-    </div>
+    <Grid
+      current={null}
+      board={[
+        {shape: 'I', x: 1, y: 1},
+        {shape: 'J', x: 1, y: 7},
+        {shape: 'L', x: 1, y: 13},
+        {shape: 'O', x: 5, y: 1},
+        {shape: 'S', x: 5, y: 5},
+        {shape: 'T', x: 5, y: 9},
+        {shape: 'Z', x: 5, y: 13}
+      ]}
+    />
   ))
